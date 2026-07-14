37d330a7464d03440130efd5ecc87e5007a20575
Reda <mahmoudreda457@gmail.com>
2026-07-14 07:09:57 +0300

    Login reactive form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit() {
+    console.log(this.form);
+    const EnteredEmail = this.form.value.email;
+    const EnteredPassword = this.form.value.password;
+
+    console.log(EnteredEmail, EnteredPassword);
+  }
+}
