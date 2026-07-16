d2137ed03310dc939b467bbdb0da6e9d84af6f08
Reda <mahmoudreda457@gmail.com>
2026-07-16 07:58:28 +0300

    Signup form

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,49 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [ReactiveFormsModule],
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.maxLength(6)],
+    }),
+    confirmPassword: new FormControl('', {
+      validators: [Validators.required, Validators.maxLength(6)],
+    }),
+    firstName: new FormControl('', { validators: [Validators.required] }),
+    lastName: new FormControl('', { validators: [Validators.required] }),
+    street: new FormControl('', { validators: [Validators.required] }),
+    number: new FormControl('', { validators: [Validators.required] }),
+    postalCode: new FormControl('', { validators: [Validators.required] }),
+    city: new FormControl('', { validators: [Validators.required] }),
+    role: new FormControl<
+      'student' | 'teahcer' | 'employee' | 'founder' | 'other'
+    >('student', { validators: [Validators.required] }),
+    agree: new FormControl(false, { validators: [Validators.required] }),
+  });
+
+  onSubmit() {
+    console.log(this.form);
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+    console.log(enteredEmail, enteredPassword);
+  }
+
+  onReset() {
+    this.form.reset();
+  }
+}
