7cac67a4363f45dc3c265bbe41c75730e88676bd
Reda <mahmoudreda457@gmail.com>
2026-07-16 08:04:07 +0300

    Nested Form Groups

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -18,18 +18,24 @@ export class SignupComponent {
     email: new FormControl('', {
       validators: [Validators.required, Validators.email],
     }),
-    password: new FormControl('', {
-      validators: [Validators.required, Validators.maxLength(6)],
+    passwords: new FormGroup({
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.maxLength(6)],
+      }),
+      confirmPassword: new FormControl('', {
+        validators: [Validators.required, Validators.maxLength(6)],
+      }),
     }),
-    confirmPassword: new FormControl('', {
-      validators: [Validators.required, Validators.maxLength(6)],
+    name: new FormGroup({
+      firstName: new FormControl('', { validators: [Validators.required] }),
+      lastName: new FormControl('', { validators: [Validators.required] }),
     }),
-    firstName: new FormControl('', { validators: [Validators.required] }),
-    lastName: new FormControl('', { validators: [Validators.required] }),
-    street: new FormControl('', { validators: [Validators.required] }),
-    number: new FormControl('', { validators: [Validators.required] }),
-    postalCode: new FormControl('', { validators: [Validators.required] }),
-    city: new FormControl('', { validators: [Validators.required] }),
+    address: new FormGroup({
+      street: new FormControl('', { validators: [Validators.required] }),
+      number: new FormControl('', { validators: [Validators.required] }),
+      postalCode: new FormControl('', { validators: [Validators.required] }),
+      city: new FormControl('', { validators: [Validators.required] }),
+    }),
     role: new FormControl<
       'student' | 'teahcer' | 'employee' | 'founder' | 'other'
     >('student', { validators: [Validators.required] }),
@@ -39,7 +45,7 @@ export class SignupComponent {
   onSubmit() {
     console.log(this.form);
     const enteredEmail = this.form.value.email;
-    const enteredPassword = this.form.value.password;
+    const enteredPassword = this.form.value.passwords?.password;
     console.log(enteredEmail, enteredPassword);
   }
 
